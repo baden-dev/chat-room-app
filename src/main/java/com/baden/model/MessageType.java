58b40199f9ad9d6e8f6800e06130b42c04608ba9
baden-dev <baden.dev@gmail.com>
2023-11-21 12:34:54 +0200

    added message type enum that defines three constant values join, chat and leave

---
+++ src/main/java/com/baden/model/MessageType.java
@@ -0,0 +1,9 @@
+package com.baden.model;
+
+public enum MessageType {
+
+    JOIN, //When a new user joins the chat
+    CHAT, //When a user sends message
+    LEAVE //When a user leaves the chat
+
+}
